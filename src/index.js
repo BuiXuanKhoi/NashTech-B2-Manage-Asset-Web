588c98ba4d4f2c95b88d95d2df3d690bb27b3c0e
Chau Van Hau <vanhau293@gmail.com>
2022-07-20 12:09:36 +0000

    Merged PR 235: Design Create user form
    Design Create user form
    
    Related work items: #1412, #1453

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CreateUser from './pages/user/CreateUser';
 
 ReactDOM.render(
-    <App />,
+    <CreateUser />,
   document.getElementById('root')
 );
 
