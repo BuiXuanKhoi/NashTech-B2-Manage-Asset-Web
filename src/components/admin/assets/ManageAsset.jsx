d4fc638551505a46baf8ac09aeefac3a5152268e
Chau Van Hau <vanhau293@gmail.com>
2022-08-02 01:43:15 +0000

    Merged PR 304: update toast when create asset
    update toast when create asset

--- src/components/admin/assets/ManageAsset.jsx
+++ src/components/admin/assets/ManageAsset.jsx
@@ -1,3 +1,16 @@
+import { Toaster } from 'react-hot-toast';
 export default function ManageAsset(){
-    return (<h1>Manage Asset</h1>);
+    return (<><h1>Manage Asset</h1>
+    <Toaster
+                toastOptions={{
+                    className: 'toast',
+                    style: {
+                        border: '1px solid #713200',
+                        padding: '36px',
+                        color: '#713200',
+                        
+                    },
+                }}
+            />
+            </>);
 }
