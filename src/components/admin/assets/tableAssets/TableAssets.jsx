494aa230178fe260eb2ba32480904ce59a5987e4
Chau Van Hau <vanhau293@gmail.com>
2022-08-03 16:00:41 +0000

    Merged PR 327: Task 1621: Integrate Delete asset
    Task 1621: Integrate Delete asset
    
    Related work items: #1418, #1621

--- src/components/admin/assets/tableAssets/TableAssets.jsx
+++ src/components/admin/assets/tableAssets/TableAssets.jsx
@@ -4,13 +4,44 @@ import {faTimesCircle, faSortDown, faPencilAlt, faSortUp} from '@fortawesome/fre
 import "antd/dist/antd.css";
 import "./TableAssets.css"
 import {ReloadOutlined, CloseCircleOutlined, LoadingOutlined} from "@ant-design/icons";
-
+import { Modal, Button } from 'antd';
 import {useNavigate} from "react-router-dom";
+import axios from 'axios';
+
+import toast, {Toaster} from "react-hot-toast";
 
 
 function TableAsset(props) {
+    
+    const loginState = JSON.parse(localStorage.getItem("loginState"));
     const [displayList, setDisplayList] = useState(props.listAsset);
     const navigate = useNavigate();
+    const [modalConfirmDelete, setModalConfirmDelete] = useState({
+        isOpen: false,
+        isLoading: false,
+    });
+    const config = {
+        headers: { Authorization: `Bearer ${loginState.token}` }
+    };
+    
+    const [modalCannotDelete, setModalCannotDelete] = useState(false);
+    const [id,setId] = useState(0);
+    const [assetName, setAssetName]=useState('');
+    const handleDelete = ()=>{
+        console.log(id);
+        axios.delete(`https://asset-assignment-be.azurewebsites.net/api/asset/`+id, config)
+            .then(
+           (response) => {
+            setModalConfirmDelete({ ...modalConfirmDelete, isOpen: false })
+            toast.success("Delete "+assetName+" successfully");
+            window.location.reload();
+            }).catch((error) => {
+                console.log(error)
+                setModalConfirmDelete({ ...modalConfirmDelete, isOpen: false })
+                setModalCannotDelete({...modalCannotDelete, isOpen: true})
+            })
+        }
+   
 
     useEffect(() => {
         setDisplayList(props.listAsset);
@@ -68,7 +99,12 @@ function TableAsset(props) {
                                                     <i className="fas fa-pencil-alt"></i>
                                                     <FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon>
                                                 </td>
-                                                <td className="btn_col_assignment delete">
+                                                <td className="btn_col_assignment delete" onClick={()=>{
+                                                    setId(item.assetId);
+                                                    setAssetName(item.assetName)
+                                                    console.log(id);
+                                                    setModalConfirmDelete({ ...modalConfirmDelete, isOpen: true })
+                                                }}>
                                                     <CloseCircleOutlined style={{color: "red"}}/>
                                                 </td>
 
@@ -77,10 +113,49 @@ function TableAsset(props) {
                                     }
                                 </tr>
                             })
+
                         }
                         </tbody>
                     </>
             }
+            
+            <Modal
+        className = "modalConfirm"
+                title="Are you sure?"
+                visible={modalConfirmDelete.isOpen}
+                width={400}
+                closable={false}
+                onOk={handleDelete}
+                onCancel = {()=> {setModalConfirmDelete({ ...modalConfirmDelete, isOpen: false })}}
+                footer={[
+                    <Button key="submit" className="buttonSave" onClick={handleDelete}>
+                     Delete
+                    </Button>,
+                    <Button key="cancel" className = "buttonCancel" onClick={()=> {setModalConfirmDelete({ ...modalConfirmDelete, isOpen: false })}}>
+                      Cancel
+                    </Button>
+                  ]}
+                
+            >
+                <p>Do you want to delete this asset?</p>
+                <br/>
+            </Modal>
+            <Modal
+                className="modalInformation"
+                title="Cannot delete asset"
+                visible={modalCannotDelete.isOpen}
+                width={400}
+                closable={true}
+                onCancel={() => {
+                    setModalCannotDelete({...modalCannotDelete, isOpen: false});
+                }}
+                footer={[]}
+            >
+                <p>Cannot delete the asset because it belongs to one or more historical assignments.
+If the asset is not able to be used anymore, please update its state in <Button type="link" onClick={()=>{navigate("/editAsset/"+id)}}><u>Edit Asset page</u></Button></p>
+                <br/>
+            </Modal>
+            
         </>
     )
 }
